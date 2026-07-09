e1ca99ef9250a3086f018812aa6a89cce43560ca
virushaV <12virusha@gmail.com>
2026-07-09 11:40:30 +0530

    All projects updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Virusha V",
+    author_email="12virusha@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
