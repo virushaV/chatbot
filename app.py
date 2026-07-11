05d5d75d732a137bb041420ace3eccc1212622cc
virushaV <12virusha@gmail.com>
2026-07-11 12:11:31 +0530

    Updated app.py for Render

--- app.py
+++ app.py
@@ -67,5 +67,6 @@ def chat():
 
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 7860, debug= True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
